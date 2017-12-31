5caf8efb11dfc9d856f51e0d42c66d2f85661f47
Jordi-Gene-Mola <32513931+Jordi-Gene-Mola@users.noreply.github.com>
2017-12-31 13:08:01 +0100

    Update feature_extractors2.py

--- Session2/src/feature_extractors2.py
+++ Session2/src/feature_extractors2.py
@@ -2,23 +2,34 @@ import cv2
 import numpy as np
 import h5py
 import os
-import cPickle
+import time
 
 def SIFT_features(SIFTdetector, train_images_filenames, train_labels):
+    if not os.path.exists('./src/descriptors/sift.npy'):
+        print 'Computing SIFT features...'
+        init=time.time()
+        Train_descriptors = []
 
-    Train_descriptors = []
-    Train_label_per_descriptor = []
+        for i in range(len(train_images_filenames)):
+            filename = train_images_filenames[i]
+            #print 'Reading image ' + filename
+            ima = cv2.imread(filename)
+            gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+            kpt, des = SIFTdetector.detectAndCompute(gray, None)
+            Train_descriptors.append(des)
 
-    for i in range(len(train_images_filenames)):
-        filename = train_images_filenames[i]
-        #print 'Reading image ' + filename
-        ima = cv2.imread(filename)
-        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
-        kpt, des = SIFTdetector.detectAndCompute(gray, None)
-        Train_descriptors.append(des)
-        Train_label_per_descriptor.append(train_labels[i])
+        Train_descriptors_array = np.asarray(Train_descriptors)
 
-    return Train_descriptors, Train_label_per_descriptor
+        np.save('./src/descriptors/sift',Train_descriptors_array)
+        end=time.time()
+        print 'Done in '+str(end-init)+' secs.'
+    else:
+        print 'Loading SIFT features...'
+        init=time.time()
+        Train_descriptors_array = np.load('./src/descriptors/sift.npy')
+        end=time.time()
+        print 'Done in '+str(end-init)+' secs.'
+    return Train_descriptors_array
 
 def SURF_features(SURFdetector, train_images_filenames, train_labels):
 
@@ -46,3 +57,4 @@ def descriptors_List2Array(descriptors):
         D[startingpoint:startingpoint+len(descriptors[i])]=descriptors[i]
         startingpoint+=len(descriptors[i])
     return D
+
